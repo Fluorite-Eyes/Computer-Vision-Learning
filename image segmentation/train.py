371654262e5c9132483e2dbf2c154ff4ab46a01e
Fluorite-Eyes <127964578+Fluorite-Eyes@users.noreply.github.com>
2023-06-16 16:55:04 +0200

    Add files via upload

---
+++ image segmentation/train.py
@@ -0,0 +1,51 @@
+from datetime import time
+import torchsummary
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import voc_loader
+import model
+import ckpt_saver
+
+
+tag = 'unet'
+num_epochs = 10
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+crop_size = (256, 256)
+
+dataloader = voc_loader.get_dataloader(10, crop_size=crop_size, shuffle=True)
+
+net = model.UNet()
+net = net.to(device)
+torchsummary.summary(net, (3, crop_size[0], crop_size[1]))
+
+csaver = ckpt_saver.CkptSaver(net, tag=tag)
+
+ce_loss = nn.CrossEntropyLoss()
+ce_loss = ce_loss.to(device)
+optim = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.0)
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(dataloader):
+        img, seg = data
+        img, seg = img.to(device), seg.to(device)
+
+        outputs = net(img)
+        loss = ce_loss(outputs, seg)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        running_loss += loss
+
+    print('[%d] loss: %.3f' % (epoch+1, running_loss / len(dataloader)))
+    if epoch % 10 == 0:
+        csaver.save(epoch)
+
+csaver.save(num_epochs)
+
+
